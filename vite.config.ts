f6107596ead152736af3c3a3cad631f6ca38723c
Angelalema <79814514+Angelalema@users.noreply.github.com>
2025-05-04 14:18:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/vetsight/', // 👈 MUY IMPORTANTE para GitHub Pages
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
